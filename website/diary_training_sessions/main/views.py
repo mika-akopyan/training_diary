01f9c0d8caabe58e55ac43559dcbfb2447ad467f
mika_akopyan <mika.akopyan2505@gmail.com>
2024-01-19 12:05:46 +0300

    feat: Add a template with a menu to the main page

--- website/diary_training_sessions/main/views.py
+++ website/diary_training_sessions/main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(requst):
-    return HttpResponse('Главная страница')
+    return render(requst, 'main/index.html')
