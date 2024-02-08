51263df71ffcc355c2e9287485e88dc87da141e8
mika_akopyan <mika.akopyan2505@gmail.com>
2024-02-08 10:46:40 +0300

    style: Customize the font for the add and edit page

--- website/diary_training_sessions/workout/forms.py
+++ website/diary_training_sessions/workout/forms.py
@@ -7,3 +7,9 @@ class ExercisesForm(forms.ModelForm):
     class Meta:
         model = Exercises
         fields = ['muscle', 'exercise', 'technique_execution', 'sides_quantity']
+        widgets = {
+            'muscle': forms.Select(attrs={'class': 'form__input'}),
+            'exercise': forms.TextInput(attrs={'class': 'form__input'}),
+            'technique_execution': forms.Textarea(attrs={'class': 'form__input'}),
+            'sides_quantity': forms.NumberInput(attrs={'class': 'form__input'})
+        }
--- website/diary_training_sessions/yoga/forms.py
+++ website/diary_training_sessions/yoga/forms.py
@@ -7,4 +7,11 @@ class AsanasForm(forms.ModelForm):
     class Meta:
         model = Asanas
         fields = ['sequence_number', 'asana', 'technique_execution', 'works_time', 'sides_quantity']
+        widgets = {
+            'sequence_number': forms.NumberInput(attrs={'class': 'form__input'}),
+            'asana': forms.TextInput(attrs={'class': 'form__input'}),
+            'technique_execution': forms.Textarea(attrs={'class': 'form__input'}),
+            'works_time': forms.TextInput(attrs={'class': 'form__input'}),
+            'sides_quantity': forms.NumberInput(attrs={'class': 'form__input'})
+        }
         
