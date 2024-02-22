6ec3922b39854fb44b70b4a2aef914766aba0e37
mika_akopyan <mika.akopyan2505@gmail.com>
2024-02-22 08:50:01 +0300

    feat: Create a registration page

--- website/diary_training_sessions/main/urls.py
+++ website/diary_training_sessions/main/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('registration/', UserRegistrationView.as_view(), name='user_registration'),
 ]
--- website/diary_training_sessions/main/views.py
+++ website/diary_training_sessions/main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from .forms import *
 
 # Create your views here.
 
 def index(requst):
     return render(requst, 'main/index.html')
+
+
+class UserRegistrationView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = 'main/user_registration.html'
+    context_object_name = 'form'
+    success_url = reverse_lazy('home')
