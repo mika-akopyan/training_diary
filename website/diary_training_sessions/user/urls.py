d1c9a44ad4489369cb214359f416dd2740eb82e1
mika_akopyan <mika.akopyan2505@gmail.com>
2024-03-04 13:26:55 +0300

    refactor: Add a namespace to a file urls.py for the user

--- website/diary_training_sessions/user/urls.py
+++ website/diary_training_sessions/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import *
 
-
+app_name = 'user'
 urlpatterns = [
     path("registration/", RegistrationView.as_view(), name="registration"),
     path("authorization/", AuthorizationView.as_view(), name="authorization"),
